dc9daf48e2629006299852f9a4568d8494d58c62
junkfood <69683722+JunkFood02@users.noreply.github.com>
2023-05-28 02:43:56 +0800

    feat: improve animation & material components

--- shared/src/commonMain/kotlin/ui/KeyBoard.kt
+++ shared/src/commonMain/kotlin/ui/KeyBoard.kt
@@ -2,7 +2,8 @@ package ui
 
 
 import androidx.compose.animation.core.Animatable
-
+import androidx.compose.animation.core.CubicBezierEasing
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
@@ -13,9 +14,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -24,12 +25,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import theme.Surfaces
 
 
 @Composable
@@ -41,98 +44,101 @@ fun KeyBoard(
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.AC,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.Clear,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.Plus,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.Minus,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
         }
         Row { // 7 8 9
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.Seven,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.Eight,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.Nine,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.Multiply,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
         }
         Row { // AC C + -
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.AC,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.Clear,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.Plus,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.Minus,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
         }
         Row { // AC C + -
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.AC,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.Clear,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.Plus,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
             KeyButton(
                 modifier = Modifier.weight(1f).aspectRatio(1f).padding(4.dp),
                 Key.Minus,
-                onKeyPressed
+                onKeyPressed = onKeyPressed
             )
         }
     }
 }
 
+const val ANIMATION_DURATION = 350
 
 @Composable
 fun KeyButton(
     modifier: Modifier = Modifier,
     key: Key,
+    containerColor: Color = Surfaces.surfaceContainer(Surfaces.STANDARD),
+    contentColor: Color = MaterialTheme.colorScheme.onSurfaceVariant,
     onKeyPressed: (Key) -> Unit
 ) {
     var isPressed by remember { mutableStateOf(false) }
@@ -146,10 +152,16 @@ fun KeyButton(
         Animatable(100f)
     }
     LaunchedEffect(isPressed) {
-        cornerPercent.animateTo(
-            if (isPressed)
-                16f else 100f,
-            animationSpec = tween(500)
+        if (isPressed) cornerPercent.animateTo(
+            targetValue = 25f,
+            animationSpec = tween(ANIMATION_DURATION, easing = CubicBezierEasing(0f, 1f, .8f, 1f))
+        )
+        else cornerPercent.animateTo(
+            targetValue = 100f,
+            animationSpec = tween(
+                ANIMATION_DURATION, easing = LinearEasing
+//            CubicBezierEasing(0f, 0f, 0f, 1f)
+            )
         )
     }
 
@@ -168,17 +180,21 @@ fun KeyButton(
                     onKeyPressed(key)
                     it.consume()
                 }
+                isPressed = false
             }
         },
-        color = MaterialTheme.colors.primary,
+        color = containerColor,
         shape = RoundedCornerShape(cornerPercent.value.toInt())
     ) {
         Box(
             contentAlignment = Alignment.Center
         ) {
-            Text(fontSize = 36.sp, text = key.text, textAlign = TextAlign.Center)
+            Text(
+                fontSize = 36.sp,
+                text = key.text,
+                textAlign = TextAlign.Center,
+                color = contentColor
+            )
         }
     }
-
-
 }
