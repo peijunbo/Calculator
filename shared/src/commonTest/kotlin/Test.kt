636d289e398b77fbf02bd768df79080c66156dee
peijunbo <peijunbo03@gmail.com>
2023-05-27 22:55:35 +0800

    Copy `Calculator.kt` & `Expression.kt` from https://github.com/Darkempire78/OpenCalc

--- shared/src/commonTest/kotlin/Test.kt
+++ shared/src/commonTest/kotlin/Test.kt
@@ -1,7 +1,9 @@
+import data.Calculator
 import data.Expression
 import data.MathOperator
 import data.evaluateInfixExpression
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class Test {
@@ -10,5 +12,141 @@ class Test {
         println(evaluateInfixExpression("√(9)/3"))
         println(evaluateInfixExpression("√((10%2)/3+4)"))
 
+    }
+}
+
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExpressionUnitTest {
+    // kotlin doesn't have location based DecimalFormatSymbols
+    private val decimalSeparatorSymbol = "."
+    private val groupingSeparatorSymbol = ","
+    @Test
+    fun percentage_isCorrect() {
+        var result = calculate("100*95%", false)
+        assertEquals(95.0, result, 0.0)
+
+        result = calculate("(10*10)*95%", false)
+        assertEquals(95.0, result, 0.0)
+
+        result = calculate("100*100%", false)
+        assertEquals(100.0, result, 0.0)
+
+        result = calculate("100*100/100*100%", false)
+        assertEquals(100.0, result, 0.0)
+
+        result = calculate("100%10", false)
+        assertEquals(10.0, result, 0.0)
+
+        result = calculate("10%10%", false)
+        assertEquals(0.01, result, 0.0)
+
+        result = calculate("900/10%", false)
+        assertEquals(9000.0, result, 0.0)
+    }
+
+    @Test
+    fun addition_isCorrect() {
+        var result = calculate("1+1", false)
+        assertEquals(2.0, result, 0.0)
+
+        result = calculate("(1+1)+1", false)
+        assertEquals(3.0, result, 0.0)
+    }
+
+    @Test
+    fun subtraction_isCorrect() {
+        var result = calculate("1-1", false)
+        assertEquals(0.0, result, 0.0)
+
+        result = calculate("1-1-1", false)
+        assertEquals(-1.0, result, 0.0)
+    }
+
+    @Test
+    fun factorial_isCorrect() {
+        var result = calculate("0!", false)
+        assertEquals(1.0, result, 0.0)
+
+        result = calculate("5!", false)
+        assertEquals(120.0, result, 0.0)
+
+        result = calculate("10!", false)
+        assertEquals(3628800.0, result, 0.0)
+
+        result = calculate("5!+5!", false)
+        assertEquals(240.0, result, 0.0)
+
+        result = calculate("(3!)!+(3!)!", false)
+        assertEquals(1440.0, result, 0.0)
+
+        result = calculate("(3!)!/(3!)!", false)
+        assertEquals(1.0, result, 0.0)
+    }
+
+    @Test
+    fun sqrt_isCorrect() {
+        var result = calculate("√2^2", false)
+        assertEquals(2.0, result, 0.0)
+
+        result = calculate("√9", false)
+        assertEquals(3.0, result, 0.0)
+    }
+
+    @Test
+    fun trigonometric_functions_isCorrect() {
+        // In radians
+        var result = calculate("cos(0)", false)
+        assertEquals(1.0, result, 0.0)
+
+        result = calculate("1*cos(0)", false)
+        assertEquals(1.0, result, 0.0)
+
+        result = calculate("1+cos(0)", false)
+        assertEquals(2.0, result, 0.0)
+
+        result = calculate("1-cos(0)", false)
+        assertEquals(0.0, result, 0.0)
+
+        result = calculate("2^cos(0)", false)
+        assertEquals(2.0, result, 0.0)
+
+        result = calculate("(cos(0))", false)
+        assertEquals(1.0, result, 0.0)
+
+        result = calculate("cos(2)", false)
+        assertEquals(-0.4161468365471424, result, 0.0)
+
+        result = calculate("tan(pi/2)", false)
+        assertEquals(Double.NaN, result, 0.0)
+
+        result = calculate("tan(45)", true)
+        assertEquals(0.9999999999999999, result, 0.0)
+
+        result = calculate("sin(220)", true)
+        assertEquals(-0.6427876096865393, result, 0.0)
+
+        result = calculate("sin(5!)", true)
+        assertEquals(0.8660254037844387, result, 0.0)
+
+        result = calculate("sin(1+1)", true)
+        assertEquals(0.03489949670250097, result, 0.0)
+    }
+
+    private fun calculate(input: String, isDegreeModeActivated : Boolean) = calculator.evaluate(expression.getCleanExpression(input, decimalSeparatorSymbol, groupingSeparatorSymbol), isDegreeModeActivated)
+
+    companion object {
+        private val expression: Expression = Expression()
+        private val calculator: Calculator = Calculator()
+
+
+//        fun setup() {
+//            expression = Expression()
+//            calculator = Calculator()
+//        }
     }
 }
