52506dbdba9f2d681e5870686793bef99821cccb
junkfood <69683722+JunkFood02@users.noreply.github.com>
2023-05-29 01:12:51 +0800

    fix: status bar color in iOS & Android

--- androidApp/src/androidMain/kotlin/com/myapplication/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/myapplication/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.myapplication
 
 import MainView
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.res.Configuration
 import android.graphics.Color
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+
+private tailrec fun Context.findWindow(): Window? =
+    when (this) {
+        is Activity -> window
+        is ContextWrapper -> baseContext.findWindow()
+        else -> null
+    }
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +28,12 @@ class MainActivity : AppCompatActivity() {
             WindowCompat.setDecorFitsSystemWindows(window, false)
             window.statusBarColor = Color.TRANSPARENT
             window.navigationBarColor = Color.TRANSPARENT
+            val window = LocalView.current.context.findWindow()
+            val view = LocalView.current
+            window?.let {
+                WindowCompat.getInsetsController(it, view).isAppearanceLightStatusBars =
+                    ((resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) == Configuration.UI_MODE_NIGHT_NO)
+            }
             MainView()
         }
     }
