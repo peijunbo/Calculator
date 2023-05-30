b1e1717052c743e23c64274b2007a3eb98a54704
peijunbo <peijunbo03@gmail.com>
2023-05-30 20:15:01 +0800

    feat: Add history drawer demo

--- shared/src/commonMain/kotlin/ui/KeyBoard.kt
+++ shared/src/commonMain/kotlin/ui/KeyBoard.kt
@@ -8,8 +8,13 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.awaitDragOrCancellation
 import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,8 +23,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
@@ -29,50 +37,122 @@ import androidx.compose.material.icons.outlined.Percent
 import androidx.compose.material.icons.outlined.Remove
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import horizontalSystemBarsPadding
 import isDeviceInPortraitMode
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import statusBarsPadding
 import theme.Surfaces
 import ui.component.AutoSizeText
 
+const val rate1 = 1f / 4f
+const val rate2 = 4f / 7f
+const val rate3 = 1f
 
 @Composable
 fun ColumnScope.KeyBoard(
-    textField: @Composable ColumnScope.() -> Unit, onKeyPressed: (Key) -> Unit = {}
+    textField: @Composable ColumnScope.() -> Unit,
+    parentHeight: Dp,
+    onKeyPressed: (Key) -> Unit = {}
 ) {
+    val offset = remember { Animatable(0f) }
+    val coroutineScope = rememberCoroutineScope()
+    val localDensity = LocalDensity.current
+    LazyColumn(modifier = Modifier.height(localDensity.run { offset.value.toDp() }),
+        reverseLayout = true) {
+        items(10) { index ->
+            Text("item$index", fontSize = 48.sp)
+        }
+    }
     Column(
         modifier = Modifier.fillMaxWidth().padding(bottom = 12.dp).clip(
             RoundedCornerShape(
                 topEnd = 0.dp, topStart = 0.dp, bottomEnd = 36.dp, bottomStart = 36.dp
             )
-        ).background(Surfaces.surfaceContainer(Surfaces.HIGHEST)).weight(3f, fill = true)
-            .statusBarsPadding().horizontalSystemBarsPadding(),
+        ).background(Surfaces.surfaceContainer(Surfaces.HIGHEST)).height(parentHeight * (3f / 7f))
+            .statusBarsPadding().horizontalSystemBarsPadding()
+            .draggable(
+                state = rememberDraggableState {
+                    println("parentHeight$parentHeight")
+                    coroutineScope.launch {
+                        offset.animateTo(
+                            targetValue = offset.targetValue + it
+                        )
+                    }
+                },
+                orientation = Orientation.Vertical,
+                onDragStopped = {
+                    val now = localDensity.run { offset.value.toDp() }
+                    val rate: Float = now / parentHeight
+                    if (rate < rate1) {
+                        if (rate > rate1 - rate) {
+                            offset.animateTo(
+                                targetValue = localDensity.run { parentHeight.toPx() * rate1 }
+                            )
+                        } else {
+                            offset.animateTo(
+                                targetValue = 0f
+                            )
+                        }
+                    } else if (rate < rate2) {
+                        if (rate - rate1 > rate2 - rate) {
+                            offset.animateTo(
+                                targetValue = localDensity.run { parentHeight.toPx() * rate2 }
+                            )
+                        } else {
+                            offset.animateTo(
+                                targetValue = localDensity.run { parentHeight.toPx() * rate1 }
+                            )
+                        }
+                    } else {
+
+                        offset.animateTo(
+                            targetValue = localDensity.run { parentHeight.toPx() * rate1 }
+                        )
+                    }
+                }
+            ),
     ) {
         textField()
     }
     Column(
-        modifier = Modifier.horizontalSystemBarsPadding().padding(horizontal = 4.dp).weight(4f)
+        modifier = Modifier.horizontalSystemBarsPadding().offset {
+            val parentPxHeight = parentHeight.toPx()
+            var yOffset = 0f
+            if (offset.value / parentPxHeight > rate1) {
+                yOffset = offset.value - parentPxHeight * rate1
+            }
+            IntOffset(0, yOffset.toInt())
+        }
+            .background(Color.Red).padding(horizontal = 4.dp)
+            .height(parentHeight * 4 / 7)
     ) {
         if (!isDeviceInPortraitMode()) {
             Row(modifier = Modifier.weight(1f)) {
