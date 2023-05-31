ea2d9d7d2b15424bacd9665ead3c360ee30a8db1
peijunbo <peijunbo03@gmail.com>
2023-05-31 15:53:45 +0800

    feat: Add input filter

--- shared/src/commonMain/kotlin/ui/Utils.kt
+++ shared/src/commonMain/kotlin/ui/Utils.kt
@@ -6,24 +6,59 @@ import androidx.compose.ui.text.input.TextFieldValue
 
 fun TextFieldValue.concatKey(key: Key): TextFieldValue {
     println("$key ${this.selection.min} -- ${this.selection.max}")
-    var index: Int = this.selection.max
+    val left = this.selection.min
+    val right = this.selection.max
+    var index: Int = right
     val newText: String = when (key) {
+        is Key.Dot -> {
+            var newString = key.string
+            if (left > 0) {
+                // cannot input dot if there is no number prefix
+                val prefix = this.text[left - 1]
+                if (!prefix.isDigit()) {
+                    newString = ""
+                }
+            }
+            index += newString.length
+            this.text.replaceRange(left, right, newString)
+
+
+        }
+
         is Key.NumberKey -> {
-            index += key.number.length
-            this.text.replaceRange(
-                this.selection.min,
-                this.selection.max,
-                key.number
-            )
+            index += key.string.length
+            this.text.replaceRange(left, right, key.string)
         }
 
         is Key.OperatorKey -> {
-            index += key.operator.length
-            this.text.replaceRange(
-                this.selection.min,
-                this.selection.max,
-                key.operator
-            )
+            if (key is Key.BinaryOperatorKey && left > 0) {
+                // if prefix is in "+-*/", replace it
+                val prefix = this.text[left - 1].toString()
+                if (
+                    prefix == Key.Plus.string
+                    || prefix == Key.Minus.string
+                    || prefix == Key.Multiply.string
+                    || prefix == Key.Division.string
+                    || prefix == Key.Power.string
+                ) {
+                    index += key.string.length - 1
+                    this.text.replaceRange(left - 1, right, key.string)
+                } else {
+                    index += key.string.length
+                    this.text.replaceRange(
+                        this.selection.min,
+                        this.selection.max,
+                        key.operator
+                    )
+                }
+            } else {
+                index += key.string.length
+                this.text.replaceRange(
+                    this.selection.min,
+                    this.selection.max,
+                    key.operator
+                )
+            }
         }
 
         is Key.Delete -> {
