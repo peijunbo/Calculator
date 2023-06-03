25b7875143f8144aa0e16a05fe43c92280694e12
peijunbo <peijunbo03@gmail.com>
2023-06-03 22:05:47 +0800

    bugfix: auto size text

--- shared/src/commonMain/kotlin/ui/component/AutoSizeText.kt
+++ shared/src/commonMain/kotlin/ui/component/AutoSizeText.kt
@@ -1,5 +1,8 @@
 package ui.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -7,9 +10,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -18,11 +23,13 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun AutoSizeText(
     text: String,
-    modifier: Modifier = Modifier,
+    scaleRate: Float = 0.618f,
+    modifier: Modifier = Modifier.fillMaxSize(),
     color: Color = Color.Unspecified,
     fontSize: TextUnit = TextUnit.Unspecified,
     fontStyle: FontStyle? = null,
@@ -30,41 +37,39 @@ fun AutoSizeText(
     fontFamily: FontFamily? = null,
     letterSpacing: TextUnit = TextUnit.Unspecified,
     textDecoration: TextDecoration? = null,
-    textAlign: TextAlign? = null,
+    textAlign: TextAlign? = TextAlign.Center,
     lineHeight: TextUnit = TextUnit.Unspecified,
     overflow: TextOverflow = TextOverflow.Clip,
     softWrap: Boolean = false,
     maxLines: Int = Int.MAX_VALUE,
     style: TextStyle = LocalTextStyle.current
 ) {
-    var readyToDraw by remember { mutableStateOf(false) }
     var scaledFontSize by remember { mutableStateOf(fontSize) }
-    Text(
-        text = text,
-        modifier = modifier.drawWithContent {
-            if (readyToDraw) {
-                drawContent()
-            }
-        },
-        color = color,
-        fontSize = scaledFontSize,
-        fontStyle = fontStyle,
-        fontWeight = fontWeight,
-        fontFamily = fontFamily,
-        letterSpacing = letterSpacing,
-        textDecoration = textDecoration,
-        textAlign = textAlign,
-        lineHeight = lineHeight,
-        overflow = overflow,
-        softWrap = softWrap,
-        maxLines = maxLines,
-        onTextLayout = { textLayoutResult ->
-            if (textLayoutResult.didOverflowHeight) {
-                scaledFontSize *= (textLayoutResult.size.height / textLayoutResult.multiParagraph.height)
-            } else {
-                readyToDraw = true
-            }
-        },
-        style = style
-    )
+    val localDensity = LocalDensity.current
+    Box(modifier = modifier.onSizeChanged {
+        val fs = localDensity.run {
+            it.height.toDp()
+        }
+        scaledFontSize = (fs.value * scaleRate).sp
+    }
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = text,
+            color = color,
+            fontSize = scaledFontSize,
+            fontStyle = fontStyle,
+            fontWeight = fontWeight,
+            fontFamily = fontFamily,
+            letterSpacing = letterSpacing,
+            textDecoration = textDecoration,
+            textAlign = textAlign,
+            lineHeight = lineHeight,
+            overflow = overflow,
+            softWrap = softWrap,
+            maxLines = maxLines,
+            style = style
+        )
+    }
+
 }
