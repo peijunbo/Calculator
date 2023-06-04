b79a8ad2fe62dee5a4266f2f0f069e947ace4d8d
peijunbo <peijunbo03@gmail.com>
2023-06-04 13:08:25 +0800

    feat: make history selectable and clickable

--- shared/src/commonMain/kotlin/ui/KeyBoard.kt
+++ shared/src/commonMain/kotlin/ui/KeyBoard.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
@@ -29,13 +30,18 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Backspace
 import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material.icons.outlined.Percent
 import androidx.compose.material.icons.outlined.Remove
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -50,6 +56,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -59,7 +66,9 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
@@ -109,11 +118,29 @@ fun ColumnScope.KeyBoard(
                     Text(dateLongToString(history.date), fontSize = 36.sp)
                 }
                 Column(
-                    modifier = Modifier.fillMaxWidth().padding(vertical = 24.dp, horizontal = 16.dp),
-                    horizontalAlignment = Alignment.End
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 24.dp),
+                    horizontalAlignment = Alignment.End,
                 ) {
-                    Text(history.expression, fontSize = 36.sp)
-                    Text(history.result, fontSize = 36.sp)
+                    SelectionContainer {
+                        Text(
+                            modifier = Modifier.fillMaxWidth().horizontalScroll(rememberScrollState()).clickable {  }.padding(horizontal = 16.dp),
+                            text = history.expression,
+                            textAlign = TextAlign.Right,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            fontSize = 36.sp
+                        )
+                    }
+                    SelectionContainer {
+                        Text(
+                            modifier = Modifier.fillMaxWidth().horizontalScroll(rememberScrollState()).clickable {  }.padding(horizontal = 16.dp),
+                            text = history.result,
+                            textAlign = TextAlign.Right,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            fontSize = 36.sp
+                        )
+                    }
                 }
             }
 
