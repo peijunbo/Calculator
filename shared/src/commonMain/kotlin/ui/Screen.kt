e417283c5a644f1d5aba6b53ba8c79a2d09c058d
peijunbo <peijunbo03@gmail.com>
2023-06-04 12:24:04 +0800

    refactor: screen ui

--- shared/src/commonMain/kotlin/ui/Screen.kt
+++ shared/src/commonMain/kotlin/ui/Screen.kt
@@ -1,10 +1,19 @@
 package ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -15,6 +24,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -24,6 +34,7 @@ import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import data.Calculator
 import data.Expression
@@ -52,13 +63,18 @@ fun Screen(
     LaunchedEffect(Unit) {
         focusRequester.requestFocus()
     }
-
+    val hideHandleColor = TextSelectionColors(
+        Color.Transparent,
+        LocalTextSelectionColors.current.backgroundColor
+    )
+    var isHandleHidden by remember { mutableStateOf(false) }
     var isError by remember { mutableStateOf(false) }
     val coroutineScope = rememberCoroutineScope()
     if (subscriptionCount == 0) { // Only launch one collector
         coroutineScope.launch {
             StateHolder.keyFlow.collect { key ->
                 isError = false
+                isHandleHidden = true
                 expression = when (key) {
                     is Key.AC -> {
                         result = ""
@@ -105,19 +121,20 @@ fun Screen(
     }
 
     CompositionLocalProvider(
-        LocalTextInputService provides null
+        LocalTextInputService provides null,
+        LocalTextSelectionColors provides if (isHandleHidden) hideHandleColor else LocalTextSelectionColors.current
     ) {
         Column(
-            modifier = Modifier.fillMaxWidth().wrapContentHeight()
+            modifier = Modifier.fillMaxWidth().padding(bottom = 12.dp).fillMaxHeight()
         ) {
             TextField(
                 modifier = Modifier.focusRequester(focusRequester),
                 textStyle = TextStyle.Default.copy(
                     fontSize = 64.sp,
-                    textAlign = TextAlign.Left
+                    textAlign = TextAlign.Right
                 ),
                 colors = TextFieldDefaults.textFieldColors(
-                    textColor = contentColor, //TODO change color on error
+                    textColor = if (isError) MaterialTheme.colorScheme.error else contentColor, //TODO change color on error
                     backgroundColor = containerColor,
                     disabledIndicatorColor = Color.Transparent,
                     errorIndicatorColor = Color.Transparent,
@@ -126,6 +143,7 @@ fun Screen(
                 ),
                 value = expression,
                 onValueChange = {
+                    isHandleHidden = false
                     println("value change")
                     expression = it
                 },
@@ -135,11 +153,11 @@ fun Screen(
             TextField(
                 modifier = Modifier.fillMaxWidth(),
                 textStyle = TextStyle(
-                    fontSize = 36.sp,
+                    fontSize = 48.sp,
                     textAlign = TextAlign.Right
                 ),
                 colors = TextFieldDefaults.textFieldColors(
-                    textColor = contentColor,
+                    textColor = if (isError) MaterialTheme.colorScheme.error else contentColor,
                     backgroundColor = containerColor,
                     disabledIndicatorColor = Color.Transparent,
                     errorIndicatorColor = Color.Transparent,
@@ -152,6 +170,12 @@ fun Screen(
                 singleLine = true,
                 isError = isError
             )
+            Spacer(modifier = Modifier.weight(1f))
+            Card(
+                modifier = Modifier.align(Alignment.CenterHorizontally).fillMaxWidth(0.08f).height(6.dp),
+                colors = CardDefaults.cardColors(containerColor = contentColor),
+                shape = RoundedCornerShape(50)
+            ) {  }
         }
 
 
